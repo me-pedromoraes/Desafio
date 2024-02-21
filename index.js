e3f5668b9a8c4d522d9a1f77e63041520ea7d494
me-pedromoraes <pedrohmsjc@gmail.com>
2024-02-20 21:52:41 -0300

    entrega do desafio

---
+++ index.js
@@ -0,0 +1,29 @@
+//variáveis
+let nome = "Bartolomeu das Quatro Carroças"
+
+//insira o nível aqui
+let nivel = 5000
+
+//códigos e condições
+if(nivel <= 1000)[
+    console.log(nome + " é patente FERRO")
+];else if(nivel > 1000 && nivel <= 2000)[
+    console.log(nome + " é patente BRONZE")
+];else if(nivel > 2000 && nivel <= 5000)[
+    console.log(nome + " é patente PRATA")
+];else if(nivel > 5000 && nivel <= 7000)[
+    console.log(nome + " é patente OURO")
+];else if(nivel > 7000 && nivel <= 8000)[
+    console.log(nome + " é patente PLATINA")
+];else if(nivel > 8000 && nivel <= 9000)[
+    console.log(nome + " é patente ASCENDENTE")
+];else if(nivel > 9000 && nivel <= 10000)[
+    console.log(nome + " é patente IMORTAL")
+];else[
+    console.log(nome + " é patente RADIANTE")
+]
+
+
+
+
+
